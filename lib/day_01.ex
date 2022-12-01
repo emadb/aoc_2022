710f73e803b4fa7970e56aacdce9429678e9b461
Emanuele DelBono <emanuele@codiceplastico.com>
2022-12-01 07:32:49 +0100

    day 1 part 1

---
+++ lib/day_01.ex
@@ -0,0 +1,20 @@
+defmodule Aoc.Day01 do
+  def part_one(input) do
+    content = InputReader.get_lines_string(input)
+
+    {_, partials} = Enum.reduce(content, {0, []}, fn x, {current, list} ->
+      case x do
+        "" -> {0, [current | list] }
+        _ -> {current + String.to_integer(x), list}
+      end
+    end)
+
+    Enum.max(partials)
+  end
+
+  def part_two(input) do
+    input
+  end
+
+
+end
---
+++ test/day_01_test.exs
@@ -0,0 +1,32 @@
+defmodule Aoc.Day01Test do
+  use ExUnit.Case
+  @test_input "./test/inputs/day_01.txt"
+  @real_input "./lib/inputs/day_01.txt"
+
+  test "part one (test)" do
+    input = Path.absname(@test_input)
+    result = Aoc.Day01.part_one(input)
+
+    assert result == 24000
+  end
+
+  test "part one" do
+    input = Path.absname(@real_input)
+    result = Aoc.Day01.part_one(input)
+    assert result == 71023
+  end
+
+  # test "part two (test)" do
+  #   input = Path.absname(@test_input)
+  #   result = Aoc.Day01.part_one(input)
+
+  #   assert result == nil
+  # end
+
+  # test "part two" do
+  #   input = Path.absname(@real_input)
+  #   result = Aoc.Day01.part_two(input)
+  #   assert result == nil
+  # end
+
+end

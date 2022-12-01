5810399fc9e0e30134a47c915f2b11fc6ebe6eef
Emanuele DelBono <emanuele@codiceplastico.com>
2022-12-01 08:56:15 +0100

    day 1: refactoring

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,18 +1,14 @@
 defmodule Aoc.Day01 do
   def part_one(input) do
-    content = InputReader.get_lines_string(input)
-
-    {_, partials} = Enum.reduce(content, {0, []}, fn x, {current, list} ->
-      case x do
-        "" -> {0, [current | list] }
-        _ -> {current + String.to_integer(x), list}
-      end
-    end)
-
-    Enum.max(partials)
+    Enum.max(chunks(input))
   end
 
   def part_two(input) do
+    [a, b, c | _] = Enum.sort(chunks(input), :desc)
+    a + b + c
+  end
+
+  defp chunks(input) do
     content = InputReader.get_lines_string(input) ++ [""]
 
     {_, partials} = Enum.reduce(content, {0, []}, fn x, {current, list} ->
@@ -21,10 +17,7 @@ defmodule Aoc.Day01 do
         _ -> {current + String.to_integer(x), list}
       end
     end)
-
-    [a, b, c | _] = Enum.sort(partials, :desc)
-    a + b + c
+    partials
   end
-
 
 end

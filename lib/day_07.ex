8445d08636455fc542098b7a47ffb9ae957e5cfd
Emanuele DelBono <emanuele@codiceplastico.com>
2022-12-08 08:37:05 +0100

    day 7 part 2

--- lib/day_07.ex
+++ lib/day_07.ex
@@ -1,84 +1,70 @@
-# defmodule Tree do
-#   defstruct files: [], parent_id: ""
-# end
-
-# defmodule File do
-#   defstruct name: "", size: 0
-# end
-
 defmodule Aoc.Day07 do
 
   def part_one(input) do
-    list = InputReader.get_lines_string(input)
-
-    {parsed_nodes, _} = Enum.reduce(list, {%{}, nil}, fn cmd, nodes ->
-      parse_command(cmd, nodes)
-    end)
-
-    node_with_local_total = parsed_nodes
-    |> Enum.reduce(parsed_nodes, fn {k, v}, nodes ->
-      new_value = Map.put_new(v, :total_size, sum_files(v.files))
-      Map.put(nodes, k, new_value)
-    end)
-
-    node_with_global_total = node_with_local_total
-    |> Enum.reduce(node_with_local_total, fn {k, v}, nodes ->
-      new_value = Map.put_new(v, :total_size_with_children, sum_files_with_children(nodes, k))
-      Map.put(nodes, k, new_value)
-    end)
-
-    node_with_global_total
-    |> Enum.filter(fn {_k, v} -> v.total_size_with_children <= 100_000 end)
-    |> Enum.map(fn {_, n} -> n.total_size_with_children end)
+    parse(input)
+    |> Map.to_list()
+    |> Enum.map(fn {_, size} -> size end)
+    |> Enum.reject(fn size -> size >= 100_000 end)
     |> Enum.sum()
-
-  end
-
-  defp sum_files_with_children(nodes, current) do
-    children_keys = Enum.filter(nodes, fn {_k, v} -> v.parent_id == current end) |> Enum.map(fn {k, _} -> k end)
-
-    Enum.reduce(children_keys, nodes[current].total_size, fn k, acc ->
-      acc + sum_files_with_children(nodes, k)
-    end)
-  end
-
-  defp sum_files(files) do
-    Enum.reduce(files, 0, fn f, acc -> acc + f.size end)
-  end
-
-  defp parse_command("$ cd /", _nodes) do
-    {%{"/" => %{files: [], parent_id: nil}}, "/"}
-  end
-
-  defp parse_command("$ ls", {nodes, current}) do
-    {nodes, current}
-  end
-
-  defp parse_command("$ cd " <> folder, {nodes, _current}) do
-    {nodes, folder}
-  end
-
-  defp parse_command("$ cd ..", {nodes, current}) do
-    {new_current, _} = Enum.find(nodes, fn {_k, v} -> v.parent_id == current end)
-    {nodes , new_current}
-  end
-
-  defp parse_command("dir " <> folder, {nodes, current}) do
-    {Map.put(nodes, folder, %{files: [], parent_id: current}) , current}
-  end
-
-  defp parse_command(cmd, {nodes, current}) do
-    [size, name] = String.split(cmd, " ")
-    new_file = %{name: name, size: String.to_integer(size)}
-
-    new_node = Map.update!(nodes, current, fn n ->
-      %{files: n.files ++ [new_file], parent_id: n.parent_id}
-    end)
-
-    {new_node, current}
   end
 
   def part_two(input) do
-    input
+    source = parse(input)
+    space_taken_up = Map.get(source, ["/"])
+    free_space = 70_000_000 - space_taken_up
+    space_to_free_up = 30_000_000 - free_space
+
+    source
+    |> Map.to_list()
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.filter(fn size -> size > space_to_free_up end)
+    |> Enum.sort(:asc)
+    |> hd()
   end
+
+  defp parse(input) do
+    File.read!(input)
+    |> String.trim()
+    |> String.split("\n")
+    |> parse_command(%{}, [])
+    |> Map.to_list()
+    |> flat_tree()
+  end
+
+  def parse_command([], tree, _), do: tree
+  def parse_command([head | tail], tree, pwd) do
+    case head do
+      "$ cd /" -> parse_command(tail, tree, ["/"])
+      "$ cd .." -> parse_command(tail, tree, tl(pwd))
+      "$ cd " <> dir -> parse_command(tail, tree, [dir | pwd])
+      "$ ls" -> parse_ls(tail, tree, pwd)
+    end
+  end
+
+  def parse_ls([], tree, pwd), do: parse_command([], tree, pwd)
+  def parse_ls([head | tail], tree, pwd) do
+    case head do
+      "$" <> _ -> parse_command([head | tail], tree, pwd)
+      "dir " <> _dir -> parse_ls(tail, tree, pwd)
+      size_name ->
+        [size, filename] = String.split(size_name, " ")
+        tuple = {filename, String.to_integer(size)}
+        new_tree = tree |> Map.update(pwd, [tuple], fn existing -> [tuple | existing] end)
+        parse_ls(tail, new_tree, pwd)
+    end
+  end
+
+  def flat_tree(tree, flat \\ %{})
+  def flat_tree([], flat), do: flat
+  def flat_tree([head | tail], flat) do
+    {path, files} = head
+    total_size = files |> Enum.map(fn {_, size} -> size end) |> Enum.sum()
+    new_flat = groups(path) |> Enum.reduce(flat, fn path, flat ->
+      Map.update(flat, path, total_size, &(&1 + total_size))
+    end)
+    flat_tree(tail, new_flat)
+  end
+
+  def groups([one]), do: [[one]]
+  def groups([_head | tail] = list), do: [list | groups(tail)]
 end
--- test/day_07_test.exs
+++ test/day_07_test.exs
@@ -10,23 +10,23 @@ defmodule Aoc.Day07Test do
     assert result == 95437
   end
 
-  # test "part one" do
-  #   input = Path.absname(@real_input)
-  #   result = Aoc.Day07.part_one(input)
-  #   assert result == nil
-  # end
+  test "part one" do
+    input = Path.absname(@real_input)
+    result = Aoc.Day07.part_one(input)
+    assert result == 1517599
+  end
 
-  # test "part two (test)" do
-  #   input = Path.absname(@test_input)
-  #   result = Aoc.Day07.part_two(input)
+  test "part two (test)" do
+    input = Path.absname(@test_input)
+    result = Aoc.Day07.part_two(input)
 
-  #   assert result == nil
-  # end
+    assert result == 24933642
+  end
 
-  # test "part two" do
-  #   input = Path.absname(@real_input)
-  #   result = Aoc.Day07.part_two(input)
-  #   assert result == nil
-  # end
+  test "part two" do
+    input = Path.absname(@real_input)
+    result = Aoc.Day07.part_two(input)
+    assert result == 2481982
+  end
 
 end

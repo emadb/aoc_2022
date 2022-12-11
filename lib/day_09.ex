52011ac13af8b6e09d87538c858eac8f1172f0a0
Emanuele DelBono <emanuele@codiceplastico.com>
2022-12-11 15:16:23 +0100

    day 9 part 2

--- lib/day_09.ex
+++ lib/day_09.ex
@@ -84,44 +84,65 @@ defmodule Aoc.Day09 do
       {dir, String.to_integer(n)}
     end)
 
-    starting_point = [
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-      %{h: {0, 0}, t: {0, 0}, moves: []},
-    ]
+    starting_point = %{
+      h1: {0, 0},
+      h2: {0, 0},
+      h3: {0, 0},
+      h4: {0, 0},
+      h5: {0, 0},
+      h6: {0, 0},
+      h7: {0, 0},
+      h8: {0, 0},
+      h9: {0, 0},
+      h0: {0, 0},
+      moves: []}
 
-    final_state = Enum.reduce(cmd, starting_point, fn d, states ->
-      move2(d, states)
-    end)
+      final_state = Enum.reduce(cmd, starting_point, fn cmd, state -> move2(cmd, state) end)
 
-    fs = List.last(final_state)
-    moves = Enum.uniq(fs.moves)
-    |> Enum.count()
-    moves + 1
+      IO.inspect final_state, label: "FINAL"
+      moves = Enum.uniq(final_state.moves) |> Enum.count()
+      moves + 1
   end
 
-  defp move2({_, 0}, states), do: states
-  defp move2({d, n}, states) do
-    {new_states, _} = Enum.reduce(states, {[], nil}, fn s, {news, prev_tail} ->
-      case prev_tail do
-        nil ->
-          new_h = move_head(d, s.h)
-          new_state = move_tail(d, %{s | h: new_h})
-          {news ++ [new_state], new_state.t}
-        _ ->
-          # new_h = move_head(d, s.h)
-          new_state = move_tail(d, %{s | h: prev_tail})
-          {news ++ [new_state], new_state.t}
+  defp move2({d, 0}, state) do
+    IO.inspect state, label: "DIRECTION #{d}"
+    state
+  end
+  defp move2({d, n}, state) do
+    new_h = move_head(d, state.h1)
+    IO.inspect new_h, label: d # {4, 1}
+    new_state = move_hn(%{state | h1: new_h}, d, :h1, :h2)
+    |> move_hn(d, :h2, :h3)
+    |> move_hn(d, :h3, :h4)
+    |> move_hn(d, :h4, :h5)
+    |> move_hn(d, :h5, :h6)
+    |> move_hn(d, :h6, :h7)
+    |> move_hn(d, :h7, :h8)
+    |> move_hn(d, :h8, :h9)
+    |> move_hn(d, :h9, :h0)
+
+    move2({d, n - 1}, new_state)
+  end
+
+  def move_hn(state, d, h, t) do
+    {xh, yh} = state[h]
+    {xt, yt} = state[t]
+
+    if neighbord?(state[h], state[t]) do
+      state
+    else
+      new_pos = case d do
+        "R" -> {xt + 1, yt}
+        "L" -> {xt - 1, yt}
+        "U" -> {xt, yt + 1}
+        "D" -> {xt, yt - 1}
       end
-    end)
-    move2({d, n - 1}, new_states)
+     if t == :h0 do
+      Map.put(%{state | moves: state.moves ++ [new_pos]}, t, new_pos)
+     else
+      Map.put(state, t, new_pos)
+     end
+    end
   end
 
 end

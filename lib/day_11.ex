90fbf5d5bda577f6b6ab899c0cf7306bac517656
Emanuele DelBono <emanuele@codiceplastico.com>
2022-12-12 10:18:11 +0100

    day 11 part 1

---
+++ lib/day_11.ex
@@ -0,0 +1,39 @@
+defmodule Aoc.Day11 do
+
+
+  def part_one(input) do
+    monkeys = input
+    1..20 |> Enum.reduce(input, fn _round, monkeys ->
+      Enum.reduce(Map.keys(monkeys), monkeys, fn k, mm ->
+        mm = Map.put(mm, k, Map.update!(mm[k], :count, &(&1 + Enum.count(mm[k].items))))
+        items_to_pass = play(mm[k])
+        mm = Map.put(mm, k, Map.put(mm[k], :items, []))
+        execute_throw(items_to_pass, mm)
+      end)
+    end)
+    |> Enum.map(fn {k, v} -> {k, v.count} end)
+    |> Enum.sort_by(fn {_, v} -> v end, :desc)
+    |> Enum.take(2)
+    |> Enum.reduce(1, fn {_, v}, acc -> v * acc end)
+
+
+  end
+
+  defp play(m) do
+    Enum.map(m.items, fn item ->
+      worry_level = floor(m.op.(item) / 3)
+      pass_to = m.test.(worry_level)
+      {pass_to, worry_level}
+    end)
+  end
+
+  defp execute_throw([], monkeys), do: monkeys
+  defp execute_throw([{dest, worry} | rest], monkeys) do
+    new_monkeys = Map.put(monkeys, dest, Map.put(monkeys[dest], :items, monkeys[dest].items ++ [worry]))
+    execute_throw(rest, new_monkeys)
+  end
+
+  def part_two(input) do
+    input
+  end
+end
---
+++ test/day_11_test.exs
@@ -0,0 +1,124 @@
+defmodule Aoc.Day11Test do
+  use ExUnit.Case
+  @test_input "./test/inputs/day_11.txt"
+  @real_input "./lib/inputs/day_11.txt"
+
+  test "part one (test)" do
+    result = Aoc.Day11.part_one(part1_test_data())
+
+    assert result == 10605
+  end
+
+  test "part one" do
+    result = Aoc.Day11.part_one(part1_real_data())
+    assert result == nil
+  end
+
+  # test "part two (test)" do
+  #   input = Path.absname(@test_input)
+  #   result = Aoc.Day11.part_two(input)
+
+  #   assert result == nil
+  # end
+
+  # test "part two" do
+  #   input = Path.absname(@real_input)
+  #   result = Aoc.Day11.part_two(input)
+  #   assert result == nil
+  # end
+
+
+  def part1_test_data() do
+    %{
+      0 => %{
+        count: 0,
+        items: [79, 98],
+        op: fn old -> old * 19 end,
+        test: fn d -> if rem(d, 23) == 0, do: 2, else: 3 end,
+        throw_to: []
+      },
+      1 => %{
+        count: 0,
+        items: [54, 65, 75, 74],
+        op: fn old -> old + 6 end,
+        test: fn d -> if rem(d, 19) == 0, do: 2, else: 0 end,
+        throw_to: []
+      },
+      2 => %{
+        count: 0,
+        items: [79, 60, 97],
+        op: fn old -> old * old end,
+        test: fn d -> if rem(d, 13) == 0, do: 1, else: 3 end,
+        throw_to: []
+      },
+      3 => %{
+        count: 0,
+        items: [74],
+        op: fn old -> old + 3 end,
+        test: fn d -> if rem(d, 17) == 0, do: 0, else: 1 end,
+        throw_to: []
+      }
+    }
+  end
+
+  def part1_real_data() do
+    %{
+      0 => %{
+        count: 0,
+        items: [54, 53],
+        op: fn old -> old * 3 end,
+        test: fn d -> if rem(d, 2) == 0, do: 2, else: 6 end,
+        throw_to: []
+      },
+      1 => %{
+        count: 0,
+        items: [95, 88, 75, 81, 91, 67, 65, 84],
+        op: fn old -> old * 11 end,
+        test: fn d -> if rem(d, 7) == 0, do: 3, else: 4 end,
+        throw_to: []
+      },
+      2 => %{
+        count: 0,
+        items: [76, 81, 50, 93, 96, 81, 83],
+        op: fn old -> old + 6 end,
+        test: fn d -> if rem(d, 3) == 0, do: 5, else: 1 end,
+        throw_to: []
+      },
+      3 => %{
+        count: 0,
+        items: [83, 85, 85, 63],
+        op: fn old -> old + 4 end,
+        test: fn d -> if rem(d, 11) == 0, do: 7, else: 4 end,
+        throw_to: []
+      },
+      4 => %{
+        count: 0,
+        items: [85, 52, 64],
+        op: fn old -> old + 8 end,
+        test: fn d -> if rem(d, 17) == 0, do: 0, else: 7 end,
+        throw_to: []
+      },
+      5 => %{
+        count: 0,
+        items: [57],
+        op: fn old -> old + 2 end,
+        test: fn d -> if rem(d, 5) == 0, do: 1, else: 3 end,
+        throw_to: []
+      },
+      6 => %{
+        count: 0,
+        items: [60, 95, 76, 66, 91],
+        op: fn old -> old * old end,
+        test: fn d -> if rem(d, 13) == 0, do: 2, else: 5 end,
+        throw_to: []
+      },
+      7 => %{
+        count: 0,
+        items: [65, 84, 76, 72, 79, 65],
+        op: fn old -> old + 5 end,
+        test: fn d -> if rem(d, 19) == 0, do: 6, else: 0 end,
+        throw_to: []
+      }
+    }
+    end
+end
